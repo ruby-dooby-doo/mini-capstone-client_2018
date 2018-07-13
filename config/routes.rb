dfc13e7f7938b630fcd8a9df38dec7ddd24531c8
Brian <riceb53@gmail.com>
2018-07-12 21:15:35 -0500

    authentication cheatsheet 1b

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
     get '/products/:id/edit' => 'products#edit'
     patch '/products/:id' => 'products#update'
     delete '/products/:id' => 'products#destroy'
+
+    get "/signup" => "users#new"
+    post "/users" => "users#create"
   end
 end
