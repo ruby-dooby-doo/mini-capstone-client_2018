a3f778eaaa5860194ad7b7b0be9af6feebde03f8
Brian <riceb53@gmail.com>
2018-07-12 19:49:31 -0500

    clean up image_url from forms and controller

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -20,7 +20,6 @@ class Client::ProductsController < ApplicationController
                      name: params[:name],
                      price: params[:price],
                      description: params[:description],
-                     image_url: params[:image_url],
                      supplier_id: params[:supplier_id]
                     }
 
