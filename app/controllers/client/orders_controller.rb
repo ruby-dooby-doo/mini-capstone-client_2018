386aaf3e2b06e099bd5125cfa7869c9038968206
Brian <riceb53@gmail.com>
2018-07-22 12:03:13 -0500

    finish shopping cart

--- app/controllers/client/orders_controller.rb
+++ app/controllers/client/orders_controller.rb
@@ -3,6 +3,8 @@ class Client::OrdersController < ApplicationController
     # get the data from the api and show it to the user
     order_id = params[:id]
     response = Unirest.get("localhost:3000/api/orders/#{order_id}")
+    p 'respnse in show action'
+    p response.body
     @order = response.body
     render "show.html.erb"
   end
@@ -10,6 +12,8 @@ class Client::OrdersController < ApplicationController
   def create
     response = Unirest.post("localhost:3000/api/orders")
     order = response.body
+    p "order in the create action"
+    p order
     redirect_to "/client/orders/#{order['id']}"
   end
 end
